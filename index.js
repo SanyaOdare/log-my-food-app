d322d05dee1401391d29b2652e17890eb57b7b63
sanyaodare@gmail.com <sanyaodare@gmail.com>
2020-09-22 17:04:43 +0100

    first commit

---
+++ index.js
@@ -0,0 +1,19 @@
+#! /usr/bin/env node
+const readline = require('readline').createInterface({
+  input: process.stdin,
+  output: process.stdout,
+})
+
+readline.question(`What would you like to log today?`, async(item) => {
+  const { data } = await axios.get(`http://localhost:3001/food`);
+  const it = data[Symbol.iterator]();
+  let position = it.next();
+  while(!position.done) {
+    const food = position.value.name;
+    if (food === item) {
+      console.log(`${item} has ${position.value.calories} calories`);
+    }
+    position = it.next();
+  }  
+  readline.close();
+});
