315f11ea2d683e5675e893708d48288902d24872
sanyaodare@gmail.com <sanyaodare@gmail.com>
2020-09-23 15:17:37 +0100

    add log food

--- index.js
+++ index.js
@@ -5,9 +5,8 @@ const readline = require('readline').createInterface({
   output: process.stdout,
   prompt: 'enter command > '
 });
-
 readline.prompt();
-readline.on('line', line => {
+readline.on('line', async line => {
   switch (line.trim()) {
     case 'list vegan foods':
       {
@@ -41,21 +40,68 @@ readline.on('line', line => {
         });        
       }
       break;
-    case 'log': {
-        readline.question(`What would you like to log today?`, async(item) => {
+    case 'log':        
           const { data } = await axios.get(`http://localhost:3001/food`);
           const it = data[Symbol.iterator]();
-          let position = it.next();
-          while(!position.done) {
-            const food = position.value.name;
-            if (food === item) {
-              console.log(`${item} has ${position.value.calories} calories`);
-            }
-            position = it.next();
-          }  
-          readline.prompt();
-        });
-      }
-  }
+          let actionIt;
 
-})
+          const actionIterator = {
+            [Symbol.iterator]() {
+              const positions = [...this.actions];
+              return {
+                [Symbol.iterator]() {
+                  return this;
+                },
+                next(...args) {
+                  if (positions.length > 0) {
+                    const position = positions.shift();
+                    const result = positions(...args);
+                    return { value: result, done: false };
+                  } else {
+                    return { done: true };
+                  }
+                },
+              };
+            },
+            actions: [askForServingSize, displayCalories],
+          };
+
+          function askForServingSize(food) {
+            readline.question(
+              `How many servings did you eat? (as a decimal: 1, 0.5, 1.25, etc.. )`,
+              servingSize => {
+                actionIt.next(servingSize, food)
+              },
+            );
+          }
+
+          function displayCalories(servingSize, food) {
+            const calories = food.calories;
+            console.log(
+              `${
+                food.name
+              } with a serving size of ${servingSize} has a ${Number.parseFloat(
+                calories * parseInt(servingSize, 10),
+                ).toFixed()} calories.`,
+              );
+              actionIt.next();
+              readline.prompt();
+          }
+
+          readline.question(`What would you like to log today?`, async(item) => {
+            let position = it.next();
+            while(!position.done) {
+              const food = position.value.name;
+              if (food === item) {
+                console.log(`${item} has ${position.value.calories} calories.`);
+                actionIt = actionIterator[Symbol.iterator]();
+                actionIt.next(position.value);
+              }
+              position = it.next();
+            }
+            readline.prompt();
+          });
+          break;
+      }
+      readline.prompt();
+});
